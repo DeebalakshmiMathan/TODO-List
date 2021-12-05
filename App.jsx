f441b9bad32d6605c2243c9ccc73980edf5c65a4
DeebalakshmiMathan <90464439+DeebalakshmiMathan@users.noreply.github.com>
2021-12-05 23:00:29 +0530

    Add files via upload
    This ToDo app Will help to Schedule Your Task made by ReactJ

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Header from './components/Header'
+import Content from './components/Content'
+import './App.css';
+
+
+const App = () => {
+    return <>
+        <Header />
+        <Content />
+       
+        
+        
+
+    </>
+}
+export default App
